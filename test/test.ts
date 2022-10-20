23384563714377d9e7f14c0dd876b4df27318ab0
Guarav Verma <gaurav.verma382001@gmail.com>
2022-10-20 11:46:19 +0530

    Added Basic Test

---
+++ test/test.ts
@@ -0,0 +1,11 @@
+import _ from "@nomiclabs/hardhat-ethers";
+import { ethers } from "hardhat";
+import { expect } from "chai";
+describe("Hello World", () => {
+    it("should get the message", async () => {
+        const HW = await ethers.getContractFactory("HelloWorld");
+        const hello = await HW.deploy();
+
+        expect(await hello.hello()).to.equal("Hello, World!!");
+    });
+});
