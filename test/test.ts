367902f3c93bd1b82e640532422cab14a2d4ae47
Guarav Verma <gaurav.verma382001@gmail.com>
2022-10-28 12:26:40 +0530

    Added :webpack: config

--- test/test.ts
+++ test/test.ts
@@ -5,6 +5,7 @@ describe("Hello World", () => {
     it("should get the message", async () => {
         const HW = await ethers.getContractFactory("HelloWorld");
         const hello = await HW.deploy();
+        await hello.deployed();
 
         expect(await hello.hello()).to.equal("Hello, World!!");
     });
